126555e0904f825e7ab452acee5911076a5d62c5
CurtMatthewGarcia <pokeminecraftexpert@gmail.com>
2026-05-25 01:10:38 +0800

    nonce: cleanup of unused and misplaced files

--- AuthenticationApi/Models/Errors/InvalidCredentialsError.cs
+++
@@ -1,8 +0,0 @@
-﻿using AuthenticationApi.Core;
-
-namespace AuthenticationApi.Models.Errors;
-
-public class InvalidCredentialsError : IError
-{
-    public string Message => "Invalid username or password";
-}
