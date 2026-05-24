30ba580fd4b85d741c0463bc53d4258ef5f3a1fb
CurtMatthewGarcia <pokeminecraftexpert@gmail.com>
2026-05-24 23:06:36 +0800

    feat: added project, core, and scaffolding

---
+++ AuthenticationApi/Core/IError.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthenticationApi.Core;
+
+/// <summary>
+/// A default infallible Error value interface for more effective error handling.
+/// </summary>
+public interface IError
+{
+    string Message { get; }
+}
